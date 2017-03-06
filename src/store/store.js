011f46b531c21b321f803ac2b98c9fcfd5f0e999
Drake Scott <dascott0204@gmail.com>
2017-03-05 22:12:26 -0600

    have to use getters and setters to v-model with vuex

--- src/store/store.js
+++ src/store/store.js
@@ -8,7 +8,8 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0
+        counter: 0,
+        value: 0
     },
     getters: {
         doubleCounter: state => {
@@ -16,6 +17,9 @@ export const store = new Vuex.Store({
         },
         stringCounter: state => {
             return state.counter + 'Clicks';
+        },
+        value: state => {
+            return state.value;
         }
     },
     mutations: {
@@ -24,6 +28,9 @@ export const store = new Vuex.Store({
         },
         decrement: (state, payload) => {
             state.counter -= payload;
+        },
+        updateValue: (state, payload) => {
+            state.value =payload;
         }
     },
     actions: {
@@ -42,6 +49,9 @@ export const store = new Vuex.Store({
             setTimeout(() => {
                 commit('decrement', payload.by);
             }, payload.duration);
+        },
+        updateValue({commit}, payload) {
+            commit('updateValue', payload)
         }
     }
 });
